89160875e961d2503a873d10cf14cf2884a31180
Tadej Strah <tadej.strah@gmail.com>
2018-05-18 20:01:58 +0200

    Initial Commit

---
+++ 2-sum-exercise2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2-sum-exercise2
+//
+//  Created by Tadej Strah on 18/05/2018.
+//  Copyright © 2018 Tadej Strah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
